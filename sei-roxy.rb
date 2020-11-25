c94121cc2ff103a65dfb013b0cee2bf00000cedb
Robino <vincent.moreno.40@gmail.com>
2020-11-25 15:33:02 +0100

    #BIHAR-2345 Mise à jour du programme

---
+++ sei-roxy.rb
@@ -0,0 +1,11 @@
+require 'poseidon'
+
+consumer = Poseidon::PartitionConsumer.new("0", "sei.kafka.pro.hupi.loc", 9092,
+                                            "sei-roxy_forcol_declenchement_workflow", 0,:earliest_offset)
+
+loop do
+  messages = consumer.fetch
+  messages.each do |m|
+    puts m.value
+  end
+end
